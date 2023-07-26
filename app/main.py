a5b5f1c31266509aeee9f84b49aede0ab8fd9a64
Kohyeonjeong <toyu7870@naver.com>
2023-07-26 13:19:09 +0900

    고현정

--- app/main.py
+++ app/main.py
@@ -10,6 +10,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_index(request: Request):
-    return templates.TemplateResponse(
-        "index.html", {"request": request, "name": "test9"}
-    )
+    return templates.TemplateResponse("index.html", {"request": request, "name": "고현정"})
